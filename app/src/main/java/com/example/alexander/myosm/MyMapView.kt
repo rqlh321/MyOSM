0046547652c3ac0308cc72cd677ad17ff6779e88
alexander <rqlh321@gmail.com>
2017-09-08 00:08:39 +0300

    tiles

--- app/src/main/java/com/example/alexander/myosm/MyMapView.kt
+++ app/src/main/java/com/example/alexander/myosm/MyMapView.kt
@@ -4,16 +4,34 @@ import android.annotation.SuppressLint
 import android.content.Context
 import com.arellomobile.mvp.MvpDelegate
 import org.osmdroid.events.MapEventsReceiver
+import org.osmdroid.tileprovider.MapTile
+import org.osmdroid.tileprovider.MapTileProviderBasic
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.MapEventsOverlay
-
+import org.osmdroid.views.overlay.TilesOverlay
 
 @SuppressLint("ViewConstructor")
 class MyMapView(context: Context, parentDelegate: MvpDelegate<*>) : MapView(context), MapEventsReceiver {
     private val markerFolderOverlay = MarkerFolderOverlay(this, parentDelegate)
 
     init {
+
+        setTileSource(object : OnlineTileSourceBase("BaseMap", 0, 18, 256, "", arrayOf("http://a.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/BaseMap/MapServer/tile/")) {
+            override fun getTileURLString(aTile: MapTile): String {
+                return baseUrl + aTile.zoomLevel + "/" + aTile.y + "/" + aTile.x + mImageFilenameEnding
+            }
+        })
+
+        val tSourceTitle: OnlineTileSourceBase = object : OnlineTileSourceBase("Anno", 0, 18, 256, "", arrayOf("http://a.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/Anno/MapServer/tile/")) {
+            override fun getTileURLString(aTile: MapTile): String {
+                return baseUrl + aTile.zoomLevel + "/" + aTile.y + "/" + aTile.x + mImageFilenameEnding
+            }
+        }
+        val tProviderTitle = MapTileProviderBasic(context, tSourceTitle)
+        val tileOverlayTitle = TilesOverlay(tProviderTitle, context)
+        overlays.add(tileOverlayTitle)
         overlays.add(markerFolderOverlay)
         overlays.add(MapEventsOverlay(this))
     }
@@ -26,6 +44,5 @@ class MyMapView(context: Context, parentDelegate: MvpDelegate<*>) : MapView(cont
         markerFolderOverlay.presenter.addMarker(p)
         return true
     }
-
 
 }
