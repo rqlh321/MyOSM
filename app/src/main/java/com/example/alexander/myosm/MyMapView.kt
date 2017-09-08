b2938499cddd673179d537a5bee952f05e2f633f
alexander <rqlh321@gmail.com>
2017-09-09 00:12:53 +0300

    tiles cadastr area

--- app/src/main/java/com/example/alexander/myosm/MyMapView.kt
+++ app/src/main/java/com/example/alexander/myosm/MyMapView.kt
@@ -17,20 +17,55 @@ class MyMapView(context: Context, parentDelegate: MvpDelegate<*>) : MapView(cont
     private val markerFolderOverlay = MarkerFolderOverlay(this, parentDelegate)
 
     init {
-
-        setTileSource(object : OnlineTileSourceBase("BaseMap", 0, 18, 256, "", arrayOf("http://a.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/BaseMap/MapServer/tile/")) {
+        maxZoomLevel=19
+        setMultiTouchControls(true)
+        setTileSource(object : OnlineTileSourceBase("BaseMap", 0, 19, 256, "", arrayOf(
+                "http://a.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/BaseMap/MapServer/tile/",
+                "http://b.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/BaseMap/MapServer/tile/",
+                "http://c.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/BaseMap/MapServer/tile/"
+        )) {
             override fun getTileURLString(aTile: MapTile): String {
                 return baseUrl + aTile.zoomLevel + "/" + aTile.y + "/" + aTile.x + mImageFilenameEnding
             }
         })
 
-        val tSourceTitle: OnlineTileSourceBase = object : OnlineTileSourceBase("Anno", 0, 18, 256, "", arrayOf("http://a.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/Anno/MapServer/tile/")) {
+        val tSourceTitle: OnlineTileSourceBase = object : OnlineTileSourceBase("Anno", 0, 19, 256, "", arrayOf(
+                "http://a.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/Anno/MapServer/tile/",
+                "http://b.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/Anno/MapServer/tile/",
+                "http://c.pkk5.rosreestr.ru/arcgis/rest/services/BaseMaps/Anno/MapServer/tile/"
+        )) {
             override fun getTileURLString(aTile: MapTile): String {
                 return baseUrl + aTile.zoomLevel + "/" + aTile.y + "/" + aTile.x + mImageFilenameEnding
             }
         }
         val tProviderTitle = MapTileProviderBasic(context, tSourceTitle)
         val tileOverlayTitle = TilesOverlay(tProviderTitle, context)
+
+        val tSourceArea: OnlineTileSourceBase = object : OnlineTileSourceBase("Area", 0, 19, 256, "", arrayOf(
+                "http://a.pkk5.rosreestr.ru/arcgis/rest/services/Cadastre/Cadastre/MapServer/export/?layers=show:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24&dpi=8&format=png32&transparent=true&bboxSR=3857&imageSR=3857&f=image&size=256,256",
+                "http://b.pkk5.rosreestr.ru/arcgis/rest/services/Cadastre/Cadastre/MapServer/export/?layers=show:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24&dpi=8&format=png32&transparent=true&bboxSR=3857&imageSR=3857&f=image&size=256,256",
+                "http://c.pkk5.rosreestr.ru/arcgis/rest/services/Cadastre/Cadastre/MapServer/export/?layers=show:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24&dpi=8&format=png32&transparent=true&bboxSR=3857&imageSR=3857&f=image&size=256,256"
+        )) {
+            override fun getTileURLString(aTile: MapTile): String {
+                val north = tile2lat(aTile.y, aTile.zoomLevel)
+                val south = tile2lat(aTile.y + 1, aTile.zoomLevel)
+                val west = tile2lon(aTile.x, aTile.zoomLevel)
+                val east = tile2lon(aTile.x + 1, aTile.zoomLevel)
+
+                val smn = SphericalMercator.lat2y(north)
+                val sms = SphericalMercator.lat2y(south)
+                val smw = SphericalMercator.lon2x(west)
+                val sme = SphericalMercator.lon2x(east)
+
+                val s = "$baseUrl&bbox=$smw,$smn,$sme,$sms"
+                println(s)
+                return s
+            }
+        }
+        val tProviderArea = MapTileProviderBasic(context, tSourceArea)
+        val tileOverlayArea = TilesOverlay(tProviderArea, context)
+
+        overlays.add(tileOverlayArea)
         overlays.add(tileOverlayTitle)
         overlays.add(markerFolderOverlay)
         overlays.add(MapEventsOverlay(this))
@@ -41,8 +76,16 @@ class MyMapView(context: Context, parentDelegate: MvpDelegate<*>) : MapView(cont
     }
 
     override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
-        markerFolderOverlay.presenter.addMarker(p)
+//        markerFolderOverlay.presenter.addMarker(p)
         return true
     }
 
+    fun tile2lon(x: Int, z: Int): Double {
+        return x / Math.pow(2.0, z.toDouble()) * 360.0 - 180
+    }
+
+    fun tile2lat(y: Int, z: Int): Double {
+        val n = Math.PI - 2.0 * Math.PI * y.toDouble() / Math.pow(2.0, z.toDouble())
+        return Math.toDegrees(Math.atan(Math.sinh(n)))
+    }
 }
